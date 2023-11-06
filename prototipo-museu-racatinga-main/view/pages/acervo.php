0b0bd86bd1c2bc825625a9f914f6ceb84b5b60ac
Pombo8 <117785799+Pombo8@users.noreply.github.com>
2023-11-06 15:07:35 -0300

    Add files via upload

---
+++ prototipo-museu-racatinga-main/view/pages/acervo.php
@@ -0,0 +1,53 @@
+<?php 
+    require "../../classes/Conexao.php";
+    require "../../classes/Acervo.php";
+    
+    $acervo = new Acervo();
+    $maxPaginas = $acervo->getMaxPaginas();
+
+    if ( isset($_GET["pagina"]) && ($_GET["pagina"] > $maxPaginas-1 || $_GET["pagina"] < 0) ){
+        header("Location: ./acervo.php?pagina=0");
+        $_GET["pagina"] = 0;
+    } else if ( !isset($_GET["pagina"]) ) {
+        $_GET["pagina"] = 0;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/acervo.css">
+    <title>Museu de Racatinga - Acervo</title>
+</head>
+<body>
+    <header>
+        <img src="../images/logo_rubrum.png" alt="logo">
+        <nav>
+            <a href="../../index.html">Home</a>
+            <a href="#">Login</a>
+        </nav>
+    </header>
+
+    <div class="navegacao-acervo">
+        <?php
+            echo "<h1>Página</h1> <div><p>", $_GET["pagina"] + 1, " de ", $maxPaginas, "</p></div>"; 
+            include "../components/barraNavegacaoAcervo.php";
+        ?>
+    </div>
+
+    <div class="wrapper-peca">
+        <?php 
+            $acervo->listar();
+        ?>
+    </div>
+
+    <div class="navegacao-acervo">
+        <?php
+            include "../components/barraNavegacaoAcervo.php";
+        ?>
+    </div>
+</body>
+</html>
---
+++ prototipo-museu-racatinga-main/view/pages/createAdm.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <form method="POST" >
+            <select name="eventos" id="eventos">
+            <?php
+                
+                $mysqli = mysqli_connect("18.230.6.129","HT301410X","HT301410X","HT301410X");
+                $query = "SELECT nome FROM Evento";
+                $result = mysqli_query($mysqli, $query);
+                $resultCheck = mysqli_num_rows($result);
+
+                if($resultCheck > 0 ){
+                    while($row = mysqli_fetch_assoc($result)){
+                        $selectedProduct = $row['nome'];
+                        echo "<option name='$selectedProduct' value='$selectedProduct'>$selectedProduct</option>";
+                    }
+                }
+            
+            ?>
+            </select><br>
+            
+            descricao <input type="text" name="desc" required><br>
+            ano <input type="text" name="ano" required><br>
+            artista <input type="text" name="artista" required><br>
+            nome <input type="text" name ="nome"required ><br>
+
+            <input type="submit" name="submit" value="Submit">     
+        </form>
+    </body>
+</html>
+<?php
+    if(isset($_POST['desc']) && isset($_POST['ano'])){
+        if(isset($_POST['artista']) && isset($_POST['nome'])){
+            require "../../classes/Conexao.php";
+            require "../../classes/Create.php";
+            $class = new Create();
+            $evento = $_POST['eventos'];
+            $desc = $_POST['desc'];
+            $ano = $_POST['ano'];
+            $artista = $_POST['artista'];
+            $nome = $_POST['nome'];
+            $class->create($evento, $desc, $ano, $artista, $nome);
+        }
+    }
+?>
